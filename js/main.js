a3935ee5a21f846a3878947292fe74703845fb7b
aloksoni11 <sonialok5777@gmail.com>
2019-05-24 20:16:07 +0530

    updat

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ window.onscroll = function () {
 };
 
 function myFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100h) {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         document.getElementById("BGeeta").className = "Bgeeta";
         document.getElementById("UGeeta").className = "Ugeeta";
     } else {
@@ -11,3 +11,12 @@ function myFunction() {
         document.getElementById("UGeeta").className = "";
     }
 }
+console.log(document.body.scrollTop);
+
+function slider() {
+    var myArray = ["1", "2", "4", "5", "6", "7", "8", "9"];
+    var x = parseInt(Math.random() * (myArray.length));
+    var sl = document.getElementById("slide");
+    sl.innerHTML = "<img src='images/" + myArray[x] + ".jpg' class='img'/>";
+}
+window.setInterval(slider, 3000);
